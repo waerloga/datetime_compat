99da5b63257d888ecd601855358abc9935d6e099
Jason Varnedoe <jason@fuzzystatic.com>
2013-09-07 23:01:01 -0400

    need DateTimeZone

---
+++ lib/DateTimeZoneCompat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 9/7/13
+ * Time: 8:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DateTimeZoneCompat {
+
+}
---
+++ test/Unit/DateTimeZoneCompatTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 9/7/13
+ * Time: 8:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DateTimeZoneCompatTest extends PHPUnit_Framework_TestCase {
+
+}
