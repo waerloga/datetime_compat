f50d0d9eed68a6ab1996931ae46478f7f22e3c2c
Jason Varnedoe <jason@fuzzystatic.com>
2013-09-07 23:03:28 -0400

    construct, format, and modify working for DateTimeCompat

--- lib/DateTimeCompat.php
+++ lib/DateTimeCompat.php
@@ -9,4 +9,62 @@
 
 class DateTimeCompat {
 
+    /** @var int */
+    protected $timestamp;
+
+    /**
+     * Returns a new DateTimeCompat object
+     *
+     * @param string $input
+     * @param DateTimeZoneCompat $timezone
+     * @throws Exception
+     * @returns DateTimeCompat
+     */
+    public function __construct($input = "now", DateTimeZoneCompat $timezone = null) {
+        $this->timestamp = strtotime($input);
+        if($this->timestamp === false || $this->timestamp == -1) {
+            throw new Exception(__CLASS__."::".__METHOD__.": Failed to parse time string (".$input.")");
+        }
+        return $this;
+    }
+
+    /**
+     * Returns date formatted according to a given format
+     * @param string $input
+     * @return string|boolean
+     */
+    public function format($input) {
+        return date($input, $this->timestamp);
+    }
+
+
+    public function modify($input) {
+        $temp = strtotime($input, $this->timestamp);
+        if($temp === false || $temp == -1) {
+            return false;
+        } else {
+            $this->timestamp = $temp;
+        }
+
+        return $this;
+    }
+
+    public function add() {
+
+    }
+
+    public function sub() {
+
+    }
+
+    public function diff() {
+
+    }
+
+    public function getTimestamp() {
+
+    }
+
+
+
 }
--- test/Unit/DateTimeCompatTest.php
+++ test/Unit/DateTimeCompatTest.php
@@ -9,14 +9,33 @@
 
 class DateTimeCompatTest extends PHPUnit_Framework_TestCase {
 
-    protected $timestamp;
+    /** @var string */
     protected $invalid;
 
+    /** @var array */
+    protected $data = array();
+
     protected function setUp() {
-        /** @var int timestamp */
-        $this->timestamp = time();
-        /** @var string invalid */
         $this->invalid = "NOTADATE";
+        $this->data['formattest'] = array(
+            'timestamp' => 1377835200,
+            'format'    => 'Y-m-d H:i:s',
+            'date'      => '2013-08-30 00:00:00'
+        );
+        $this->data['modifytest1'] = array(
+            'timestamp' => 1377835200,
+            'startdate' => '2013-08-30 00:00:00',
+            'format'    => 'Y-m-d H:i:s',
+            'modify'    => '+2 day',
+            'date'      => '2013-09-01 00:00:00'
+        );
+        $this->data['modifytest2'] = array(
+            'timestamp' => 1377835200,
+            'startdate' => '2013-08-30 00:00:00',
+            'format'    => 'Y-m-d H:i:s',
+            'modify'    => '-1 day',
+            'date'      => '2013-08-29 00:00:00'
+        );
     }
 
     /**
@@ -24,8 +43,7 @@ class DateTimeCompatTest extends PHPUnit_Framework_TestCase {
      * @covers DateTimeCompat::__construct
      */
     public function constructReturnsDateTimeCompat() {
-        $dummy = new DateTimeCompat();
-        $this->assertInstanceOf("DateTimeCompat", $dummy);
+        $this->assertInstanceOf("DateTimeCompat", new DateTimeCompat());
     }
 
     /**
@@ -37,5 +55,51 @@ class DateTimeCompatTest extends PHPUnit_Framework_TestCase {
         $dummy = new DateTimeCompat($this->invalid);
     }
 
+    /**
+     * @test
+     * @covers DateTimeCompat::format
+     */
+    public function formatReturnsDateTimeString() {
+        $dummy = new DateTimeCompat($this->data['formattest']['date']);
+        $this->assertEquals($this->data['formattest']['date'],$dummy->format($this->data['formattest']['format']));
+    }
+
+    /**
+     * @test
+     * @covers DateTimeCompat::modify
+     */
+    public function modifyReturnsFalseOnInvalid() {
+        $dummy = new DateTimeCompat($this->data['modifytest1']['startdate']);
+        $this->assertFalse($dummy->modify($this->invalid));
+    }
+
+    /**
+     * @test
+     * @covers DateTimeCompat::modify
+     */
+    public function modifyReturnsObjectOnValid() {
+        $dummy = new DateTimeCompat($this->data['modifytest1']['startdate']);
+        $this->assertInstanceOf("DateTimeCompat", $dummy->modify($this->data['modifytest1']['modify']));
+    }
+
+    /**
+     * @test
+     * @covers DateTimeCompat::modify
+     */
+    public function modifyAddsProperly() {
+        $dummy = new DateTimeCompat($this->data['modifytest1']['startdate']);
+        $dummy->modify($this->data['modifytest1']['modify']);
+        $this->assertEquals($this->data['modifytest1']['date'],$dummy->format($this->data['modifytest1']['format']));
+    }
+
+    /**
+     * @test
+     * @covers DateTimeCompat::modify
+     */
+    public function modifySubsProperly() {
+        $dummy = new DateTimeCompat($this->data['modifytest2']['startdate']);
+        $dummy->modify($this->data['modifytest2']['modify']);
+        $this->assertEquals($this->data['modifytest2']['date'],$dummy->format($this->data['modifytest2']['format']));
+    }
 
 }
