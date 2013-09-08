1e463eb6a443dacc0567f13cc6479284a3803a66
Jason Varnedoe <jason@fuzzystatic.com>
2013-09-08 12:56:30 -0400

    added barebones datetimezonecompat functionality

--- lib/DateTimeZoneCompat.php
+++ lib/DateTimeZoneCompat.php
@@ -9,4 +9,101 @@
 
 class DateTimeZoneCompat {
 
+    /** @var string */
+    protected $system_tz;
+    /** @var string */
+    protected $name;
+
+    public function __construct($input) {
+        if(ini_get('date.timezone')) {
+            $this->system_tz = ini_get('date.timezone');
+        } else {
+            $this->system_tz = date_default_timezone_get();
+        }
+
+        if(!$this->setName($input)) {
+            throw new Exception(__CLASS__ . '::' . __METHOD__ . ': Unkonwn or bad timezone (' . $input . ')');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets the TZ Name (checking to see if it's valid first)
+     *
+     * @param $input
+     * @return bool
+     */
+    protected function setName($input) {
+        if(!date_default_timezone_set($input)) {
+            return false;
+        }
+        $this->name = $input;
+        date_default_timezone_set($this->system_tz);
+
+        return true;
+    }
+
+    /**
+     * Returns the name of the timezone
+     *
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * Returns the offset to UTC for the input date/time in seconds.
+     *
+     * @param DateTimeCompat $input
+     * @return int
+     */
+    public function getOffset(DateTimeCompat $input) {
+        date_default_timezone_set($this->getName());
+        $temp = date('Z', $input->getTimestamp());
+        date_default_timezone_set($this->system_tz);
+        if($temp) {
+            return (int)$temp;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns location information for a timezone
+     *
+     * @return array
+     * @todo make it go
+     */
+    public function getLocation() {
+
+    }
+
+
+    public function getTransitions() {
+
+    }
+
+    /**
+     * Returns associated array containing dst, offset, and the timezone name
+     *
+     * @return array
+     * @todo make it go
+     */
+    public static function listAbbreviations() {
+
+    }
+
+    /**
+     * Returns a numerically indexed array containing all defined timezone identifiers
+     *
+     * @return array
+     * @todo make it go
+     */
+    public static function listIdentifiers() {
+
+    }
+
+
 }
--- test/Unit/DateTimeZoneCompatTest.php
+++ test/Unit/DateTimeZoneCompatTest.php
@@ -9,12 +9,71 @@
 
 class DateTimeZoneCompatTest extends PHPUnit_Framework_TestCase {
 
+    protected $sys_tz;
+    protected $invalid;
+    protected $valid = array();
+
+    protected function setUp() {
+        if(ini_get('date.timezone')) {
+            $this->sys_tz = ini_get('date.timezone');
+        } else {
+            $this->sys_tz = date_default_timezone_get();
+        }
+
+        $this->invalid = 'Murica/Texas';
+
+        $this->valid['construct'] = array(
+            'name'       => 'America/Chicago',
+            'offset_dst' => '-18000',
+            'offset'     => '-21600'
+        );
+    }
+
     /**
      * @test
      * @covers DateTimeZoneCompat::__construct
      */
     public function constructReturnsDateTimeCompat() {
-        $this->assertInstanceOf("DateTimeZoneCompat", new DateTimeZoneCompat());
+        $this->assertInstanceOf("DateTimeZoneCompat", new DateTimeZoneCompat($this->valid['construct']['name']));
+    }
+
+    /**
+     * @test
+     * @covers DateTimeZoneCompat::__construct
+     * @expectedException Exception
+     */
+    public function constructThrowsExceptionOnInvalid() {
+        $dummy = new DateTimeZoneCompat($this->invalid);
+    }
+
+    /**
+     * @test
+     * @covers DateTimeZoneCompat::getName
+     */
+    public function getNameReturnsExpcected() {
+        $dummy = new DateTimeZoneCompat($this->valid['construct']['name']);
+        $this->assertEquals($this->valid['construct']['name'], $dummy->getName());
+    }
+
+    /**
+     * @test
+     * @covers DateTimeZoneCompat::getOffset
+     */
+    public function getOffsetReturnsExpcected() {
+        $dummy = new DateTimeZoneCompat($this->valid['construct']['name']);
+        $time = new DateTimeCompat();
+
+        /** for this to work, must set to the expected timezone ourselves and see if we are in DST */
+        date_default_timezone_set($this->valid['construct']['name']);
+        if(date('I')) {
+            $offset = $this->valid['construct']['offset_dst'];
+        } else {
+            $offset = $this->valid['construct']['offset'];
+        }
+        date_default_timezone_set($this->sys_tz);
+
+
+        $this->assertEquals($offset, $dummy->getOffset($time));
     }
 
 }
